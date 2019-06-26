d800b6d6e3cdeec7f75774f9d5ae01a572ee5e45
jawher ben cheikh brahim <jbencheikhbrahim@mcsp.gov.tn>
2019-06-26 15:31:42 +0100

    first commit

---
+++ src/main/java/com/inv/mac/GestionInventaireMacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inv.mac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionInventaireMacBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionInventaireMacBackendApplication.class, args);
+	}
+
+}
